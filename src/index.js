98868d92bd46f2dd67d05ec7139af249c8063b01
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-01-06 19:54:16 +0530

    login implementation (toast-notificatios)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ToastProvider } from 'react-toast-notifications';
 import './styles/index.css';
 import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App/>
-    </Router>
+    
+      <ToastProvider autoDismiss autoDismissTime = {5000} placement = "top-left">
+        <Router>
+          <App/>
+        </Router>
+      </ToastProvider>
+    
   </React.StrictMode>
 );
